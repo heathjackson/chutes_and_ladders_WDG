34de0f4f6d7a36ed602e5340a9102f2623e1ab25
Heather <80366350+heathjackson@users.noreply.github.com>
2024-03-12 23:13:48 -0400

    rules refactored

--- apps/games-ui/src/components/game_info.tsx
+++ apps/games-ui/src/components/game_info.tsx
@@ -7,15 +7,23 @@ const GameInfo = () => {
   const {id}  = useParams();
   const games = useLoaderData() as Array<IGameInfo>
   const gameInfo = games.find(({name}) => name === id)
-  const rules = gameInfo?.rules[0]
+  const rules = gameInfo?.rules
+
+  const rulesMap = rules?.map((rule) => {
+    return(
+      <div>
+        <h2>{rule.title}</h2>
+        <div>{rule.value}</div>
+      </div>
+    )
+  })
 
   return(
     <div>
-      <h2>Rules for {gameInfo?.name}</h2>
+      <h1>Rules for {gameInfo?.name}</h1>
       <ul>
         <li>{gameInfo?.description}</li>
-        <h4>{rules?.title}</h4>
-        <li>{rules?.value}</li>
+          {rulesMap}
         <img src={gameInfo?.imageURL} alt="Game"/>
       </ul>
       <Button variant="contained">Contained</Button>
