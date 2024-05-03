865f373ca23ac7522eece940aec85765532eaceb
Heather <80366350+heathjackson@users.noreply.github.com>
2024-05-03 00:46:25 -0400

    working with ui and registration

--- apis/games-api/jest.config.ts
+++ apis/games-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'model',
+  displayName: 'games-api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': '@swc/jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/games-ui',
+  coverageDirectory: '../../coverage/apis/games-api',
 };
