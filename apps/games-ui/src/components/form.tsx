fcc47dfaf6dcb4d3e60b5782d523bede53d94d64
Heather <80366350+heathjackson@users.noreply.github.com>
2024-07-07 15:54:13 -0400

    formik working with drop down menu

--- apps/games-ui/src/components/form.tsx
+++ apps/games-ui/src/components/form.tsx
@@ -1,58 +1,50 @@
-import { Container } from "@mui/material";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { useSubmit, useParams } from "react-router-dom";
 import * as Yup from "yup";
 
-
 const schema = Yup.object({
   userName: Yup.string().required("Please enter a valid name"),
-  color: Yup.string().required("Require"),
-})
+  color: Yup.string().required("Required"),
+});
 
+export const Register = () => {
+  const id = useParams().id;
+  const submit = useSubmit();
 
-export const Register = () =>  {
-  const id = useParams().id
-  const submit = useSubmit()
+  // const colorOptions = [
+  //   { value: "red", label: "Red" },
+  //   { value: "green", label: "Green" },
+  //   { value: "blue", label: "Blue" },
+  //   // Add more color options as needed
+  // ];
 
-  const {values, errors, handleBlur, handleChange, handleSubmit, touched} = useFormik({
-    initialValues: {
+  return (
+   <Formik
+    initialValues = {{
       userName: "",
-      color: "",
-    },
-    validationSchema: schema,
-    onSubmit: async(values) => {
-      submit(values, {method: "post", action: `/games/${id}/playGame`})
-    },
-  });
-
-  return(
-    <Container>
-      <form onSubmit={handleSubmit}>
+      color: ""
+    }}
+    validationSchema={schema}
+    onSubmit={async (values) => {
+      submit(values, { method: "post", action: `/games/${id}/playGame` });
+    }}
+    >
+      <Form>
         <label htmlFor="userName">User Name</label>
-        <input 
-          value={values.userName}
-          onChange={handleChange} 
-          id="userName"
-          type="text" 
-          placeholder="Enter your name"
-          onBlur={handleBlur}
-          className={errors.userName && touched.userName ? "input-error" : ""}/>
-        {errors.userName && touched.userName && <p className= "error">{errors.userName}</p>}
+        <Field name="userName" type="text" className="form-input" placeholder="Put in your name"/>
+        <ErrorMessage name="userName"/>
+  
+        <label htmlFor="color">Color</label>
+        <Field name="color" as="select" className="my-select">
+          <option value="null">Please Choose a Color</option>
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+        </Field>
+        <ErrorMessage name="color"/>
 
-        <label htmlFor="color">color</label>
-        <input 
-          value={values.color}
-          onChange={handleChange} 
-          id="color"
-          type="text" 
-          placeholder="Enter color"
-          onBlur={handleBlur}
-          className={errors.color && touched.color ? "input-error" : ""}/>
-        {errors.color && touched.color && <p className= "error">{errors.color}</p>}
-      
-          <button type="submit">Submit</button>
-
-      </form>
-    </Container>
-  )
-}
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
