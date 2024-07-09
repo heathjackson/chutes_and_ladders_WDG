f61403b6fda62605de481506633dc4f689b1faac
Heather <80366350+heathjackson@users.noreply.github.com>
2024-07-09 15:54:07 -0400

    added allowJs to tsconfig files

--- apis/games-api-e2e/src/support/test-setup.ts
+++ apis/games-api-e2e/src/support/test-setup.ts
@@ -8,12 +8,3 @@ module.exports = async function () {
   const port = process.env.PORT ?? '3000';
   axios.defaults.baseURL = `http://${host}:${port}`;
 };
-
-describe('#getListGames', () => {
-  test.skip('gets a list of games playable', async () => {
-    const res = await axios.get('http://localhost:3333/api/v1/games');
-
-    const gameList = res.data;
-    expect(gameList).toBeTruthy();
-  });
-});
