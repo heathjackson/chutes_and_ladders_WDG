aa363d5e53691a1943d582ed1f2433b071ae1a28
Heather <80366350+heathjackson@users.noreply.github.com>
2024-07-24 23:43:33 -0400

    new api folders created

--- apis/games-apis/games-api/jest.config.ts
+++ apis/games-apis/games-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'games-api',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': '@swc/jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apis/games-api',
+  coverageDirectory: '../../../coverage/apis/games-apis/games-api',
 };
